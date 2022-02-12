corrected magnification error and now PSF calculation seems to work

scripts/2022_02_07_ray_trace_ideal_opm_psf.py
@@ -2,15 +2,17 @@
 Calculate ideal PSF of "snouty" oblique plane microscope (OPM)
 """
 import time
+from pathlib import Path
 import numpy as np
 from numpy import fft
 from scipy.interpolate import griddata
+import tifffile
 import raytrace.raytrace as rt
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.colors import PowerNorm
 
-plot_results = True
+plot_results = False
 
 # parameters
 wavelength = 532e-6
@@ -49,7 +51,7 @@ o3_normal = np.array([-np.sin(theta), 0, np.cos(theta)])
 f_tube_lens_3 = 200
 
 # total magnification
-total_mag = mag1 / mag2 * mag3
+total_mag = remote_mag * mag3
 
 # positions
 p_o1 = n1 * f1 # O1 position
@@ -72,7 +74,7 @@ surfaces = [rt.perfect_lens(f1, [0, 0, p_o1], [0, 0, 1], alpha1),  # O1
             rt.perfect_lens(f2, [0, 0, p_o2], [0, 0, 1], alpha2),  # O2
             rt.flat_surface([0, 0, p_remote_focus], o3_normal, r2), # snouty nose cone
             rt.perfect_lens(f3, p_o3, o3_normal, alpha3),  # O3
-            rt.flat_surface(p_pupil_o3, o3_normal, f3*n3), # pupil of 03
+            rt.flat_surface(p_pupil_o3, o3_normal, r3), # pupil of 03
             rt.perfect_lens(f_tube_lens_3, p_t3, o3_normal, alpha3),  # tube lens #3
             rt.flat_surface(p_imag, o3_normal, aperture_rad)]
 ns = [n1, 1, 1, 1, 1, 1, n2, n3, 1, 1, 1, 1]
@@ -104,32 +106,26 @@ extent_xy_out = [e / total_mag for e in extent_xy_out]
 # launch rays and ray trace
 tstart = time.perf_counter()
 
-npos = 21
-# dx_pos = 200e-6
-n_shift_factor = 10
-dx_pos = dxy / total_mag * np.cos(theta) * n_shift_factor
+npos = 201
+dx_pos = dxy_out / total_mag * np.cos(theta)
 xs = dx_pos * np.arange(npos, dtype=float)
 xs -= np.mean(xs)
+# xs = np.array([0.001])
 
 output_efield = np.zeros((npos, nxy, nxy), dtype=complex)
 pupil_efield = np.zeros((npos, nxy, nxy), dtype=complex)
 for ii in range(npos):
     print("ray tracing z-plane %d/%d, elapsed time %0.2fs" % (ii + 1, npos, time.perf_counter() - tstart), end="\r")
 
-    # dx =
-    dy = 0.00
-    dz = 0.00
-    # dz = xs[ii] * np.tan(theta)
-    # rays = rt.get_ray_fan([dx, dy, dz], alpha1, 31, wavelength=wavelength, nphis=21)
-    rays = rt.get_ray_fan([xs[ii], dy, dz], alpha1, 101, wavelength=wavelength, nphis=51)
-    # rays = rt.get_collimated_rays([dx, dy, dz], 1, 101, wavelength=wavelength, nphis=51,
-    #                               normal=[0, np.sin(10*np.pi/180), np.cos(10*np.pi/180)])
+    # zpos = xs[ii] * np.tan(theta)
+    zpos = 0
+    rays = rt.get_ray_fan([xs[ii], 0, zpos], alpha1, 101, wavelength=wavelength, nphis=51)
     rays = rt.ray_trace_system(rays, surfaces, ns)
 
     # ##################################
     # plot ray trace surfaces
     # ##################################
-    if False:
+    if ii == (npos // 2):
         rt.plot_rays(rays, surfaces)
         ax = plt.gca()
         ax.axis("equal")
@@ -137,20 +133,6 @@ for ii in range(npos):
         # draw imaging plane
         l = 1
         ax.plot([-np.sin(theta) * l, np.sin(theta) * l], [-np.cos(theta) * l, np.cos(theta) * l], 'k')
-
-    # ##################################
-    # plot initial angles and remote volume angles
-    # checking that sine condition is satisfied
-    # ##################################
-    # init_angle = np.arctan(rays[0, :, 3] / rays[0, :, 5])
-    # remote_angle = np.arctan(rays[10, :, 3] / rays[10, :, 5])
-    #
-    # figh = plt.figure()
-    # ax = plt.subplot(1, 1, 1)
-    # ax.plot(init_angle * 180/np.pi, remote_angle * 180/np.pi)
-    # ax.plot(init_angle * 180/np.pi, init_angle * 180/np.pi, 'rx')
-    # ax.set_xlabel("initial angle (deg)")
-    # ax.set_ylabel("remote angle (deg)")
 
     # ##################################
     # plot rays in O1 pupil
@@ -173,10 +155,17 @@ for ii in range(npos):
     # plot rays in 03 pupil
     # ##################################
     rays_pupil_o3 = rays[-5]
-    na = np.array([1, 0, 0])
+
+    # basis vectors
+    na = np.array([np.cos(theta), 0, np.sin(theta)])
     nc = surfaces[-1].normal
+
     nb = np.cross(nc, na)
+    nb = nb / np.linalg.norm(nb)
+    # surface center
     c = surfaces[-3].center
+
+    # x-like vectors
     x_o3 = np.sum((rays_pupil_o3[:, :3] - np.expand_dims(c, axis=0)) * np.expand_dims(na, axis=0), axis=1)
     y_o3 = np.sum((rays_pupil_o3[:, :3] - np.expand_dims(c, axis=0)) * np.expand_dims(nb, axis=0), axis=1)
     phi_o3 = rays_pupil_o3[:, -2]
@@ -198,9 +187,9 @@ for ii in range(npos):
     # ##################################
     # plot results
     # ##################################
-    if plot_results:
+    if plot_results or ii == (npos // 2):
         figh = plt.figure()
-        plt.suptitle("input position = (x, y, z) = (%0.5f, %0.5f, %0.5f)" % (xs[ii], dy, dz))
+        plt.suptitle("input position = (x, y, z) = (%0.5f, 0, 0)" % xs[ii])
         grid = plt.GridSpec(2, 3)
 
         # O1 pupil
@@ -257,7 +246,7 @@ for ii in range(npos):
         ax.set_title("interpolated arg(E) O3")
         ax.imshow(phis_interp, cmap="hsv", vmin=np.nanmin(phi_o3), vmax=np.nanmax(phi_o3),
                   extent=extent_xy_pupil, origin="lower")
-        ax.add_artist(Circle((0, 0), radius=r1, color='k', fill=False))
+        ax.add_artist(Circle((0, 0), radius=r3, color='k', fill=False))
         ax.set_xlabel("position along $n_a$ (mm)")
         ax.set_ylabel("position along $n_b$ (mm)")
         ax.set_ylim([-r3, r3])
@@ -275,39 +264,96 @@ for ii in range(npos):
         ax.set_ylabel("$y$-position (um)")
 print("")
 
-psf = np.abs(output_efield)**2
-psf = psf / np.max(psf)
+for ii in range(0, npos, 20):
+    figh = plt.figure()
+    plt.suptitle("imaged %d, x = %0.3fum" % (ii, xs[ii] / 1e-3))
+    ax = plt.subplot(1, 1, 1)
+    ax.imshow(np.abs(output_efield[ii])**2, extent=extent_xy_out_um, origin="lower", cmap="bone")
+    ax.set_xlim([-4, 4])
+    ax.set_ylim([-2, 2])
+    ax.set_xlabel("na position (um)")
+    ax.set_ylabel("nb position (um)")
+
 
 # ##################################
 # convert psf to coverslip frame
 # ##################################
-i0 = xs / dx_pos
-i1 = np.arange(nxy) - nxy//2
-i2 = np.arange(nxy) - nxy//2
+no, n1, n2 = output_efield.shape
+n1_red = 31
+psf_coverslip = np.zeros((no + n2, n1_red, n2))
+for ii in range(n2):
+    psf_coverslip[ii : ii + no, :, ii] = np.abs(output_efield[:, nxy//2 - n1_red//2:nxy//2 + n1_red//2 + 1, ii])**2
+    # psf_coverslip[-(ii + 1 + no): -(ii + 1), :, ii] = np.abs(output_efield[:, nxy//2 - n1_red//2:nxy//2 + n1_red//2 + 1, ii])**2
 
-x_coverslip = dx_pos * i0[:, None, None] + dxy * np.cos(theta) * i1[None, :, None]
-y_coverslip = dxy * np.cos(theta) * i1[None, :, None]
-z_coverslip = dxy * np.sin(theta) * i2[None, None, :]
+psf_coverslip /= np.max(psf_coverslip)
 
-# todo: interpolate on some reasonable xyz grid
-# z_cgrid, y_cgrid, x_cgrid = np.meshgrid()
-# pts = np.stack((x_o3[to_use], y_o3[to_use]), axis=1)
-#
-# interp_pts = np.stack((xx_pupil.ravel(), yy_pupil.ravel()), axis=1)
-# phis_interp = griddata(pts, phi_o3[to_use], interp_pts).reshape(xx_pupil.shape)
+# note that X and Y are swapped compared with my usual way of thinking about this...
+# i0' -> X; i1' -> Y; i2' -> Z
+dx = np.cos(theta) * dxy_out / total_mag
+dy = dxy_out / total_mag
+dz = np.sin(theta) * dxy_out / total_mag
+
+xs_cs = np.arange(no + n2) * dx
+xs_cs -= np.mean(xs_cs)
+ys_cs = np.arange(n1_red) * dy
+ys_cs -= np.mean(ys_cs)
+zs_cs = np.arange(n2) * dz
+zs_cs -= np.mean(zs_cs)
+
+# put psf_coverslip in z, y, x order
+psf_coverslip = psf_coverslip.transpose([2, 1, 0])
+
+# in um
+extent_xy_coverslip = [xs_cs[0] - 0.5 * dx, xs_cs[-1] + 0.5 * dx,
+                       ys_cs[0] - 0.5 * dy, ys_cs[-1] + 0.5 * dy]
+extent_xy_coverslip = [e / 1e-3 for e in extent_xy_coverslip]
+
+extent_yz_coverslip = [ys_cs[0] - 0.5 * dy, ys_cs[-1] + 0.5 * dy,
+                       zs_cs[0] - 0.5 * dz, zs_cs[-1] + 0.5 * dz]
+extent_yz_coverslip = [e / 1e-3 for e in extent_yz_coverslip]
+
+extent_xz_coverslip = [xs_cs[0] - 0.5 * dx, xs_cs[-1] + 0.5 * dx,
+                       zs_cs[0] - 0.5 * dz, zs_cs[-1] + 0.5 * dz]
+extent_xz_coverslip = [e / 1e-3 for e in extent_xz_coverslip]
 
 # ##################################
 # plot PSF
 # ##################################
 figh = plt.figure()
-plt.suptitle("PSF")
+plt.suptitle("PSF, coverslip")
 
-ax = plt.subplot(2, 2, 1)
-ax.imshow(np.max(psf, axis=2), aspect="auto", origin="lower", cmap="bone", interpolation="none")
-ax.set_xlabel("I1")
-ax.set_ylabel("I0")
+len_scale = 0.8
 
-ax = plt.subplot(2, 2, 2)
-ax.imshow(np.max(psf, axis=1), aspect="auto", origin="lower", cmap="bone", interpolation="none")
-ax.set_xlabel("I2")
-ax.set_ylabel("I0")
+ax = plt.subplot(1, 3, 1)
+ax.imshow(np.max(psf_coverslip, axis=0), aspect="equal", origin="lower", cmap="bone", interpolation="none",
+          extent=extent_xy_coverslip)
+ax.set_xlabel("X (um)")
+ax.set_ylabel("Y (um)")
+ax.set_xlim([-len_scale, len_scale])
+ax.set_ylim([-len_scale, len_scale])
+
+ax = plt.subplot(1, 3, 2)
+ax.imshow(np.max(psf_coverslip, axis=1), aspect="equal", origin="lower", cmap="bone", interpolation="none",
+          extent=extent_xz_coverslip)
+ax.set_xlabel("X (um)")
+ax.set_ylabel("Z (um)")
+ax.set_xlim([-len_scale, len_scale])
+ax.set_ylim([-len_scale, len_scale])
+
+ax = plt.subplot(1, 3, 3)
+ax.imshow(np.max(psf_coverslip, axis=2), aspect="equal", origin="lower", cmap="bone", interpolation="none",
+          extent=extent_yz_coverslip)
+ax.set_xlabel("Y (um)")
+ax.set_ylabel("Z (um)")
+ax.set_xlim([-len_scale, len_scale])
+ax.set_ylim([-len_scale, len_scale])
+
+# save results in tif
+tifffile.imwrite(Path(r"C:\Users\q2ilab\Desktop\snouty_psf.tif"),
+                 tifffile.transpose_axes(psf_coverslip.astype(np.float32), "ZYX", asaxes="TZCYXS"),
+                 imagej=True,
+                 resolution=(1 / (dx * 1e3), 1 / (dy * 1e3)),
+                 metadata={"Info": "snouty psf, theta = %0.2fdeg" % (theta * 180/np.pi),
+                           "unit": "um", "spacing": (dz * 1e3)})
+
+